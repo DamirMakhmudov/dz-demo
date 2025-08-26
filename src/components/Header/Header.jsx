d0f54a6dcee126c2202417594380860ba134b0e0
DamirMakhmudov <samrukov@gmail.com>
2025-08-26 19:49:21 +0300

    first blood

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+function Title({ text }) {
+  return (
+    <div className='header'>{text}</div>
+  );
+}
+
+export default Title;
---
+++ src/components/Paragraph/Paragraph.jsx
@@ -0,0 +1,9 @@
+import './Paragraph.css';
+
+function Paragraph({ width, height, fontSize, text }) {
+  return (
+    <div className='paragraph' style={{ width, height, fontSize }}>{text}</div>
+  );
+}
+
+export default Paragraph;
